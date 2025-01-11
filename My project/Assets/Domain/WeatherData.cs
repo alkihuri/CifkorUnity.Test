6ba167fed3b828eeab460013faa28395d631328e
alkihuri <alkihuri@gmail.com>
2025-01-11 23:17:07 +0100

    model weather data

---
+++ My project/Assets/Domain/WeatherData.cs
@@ -0,0 +1,25 @@
+[System.Serializable]
+public class WeatherData
+{
+    public Properties properties;
+}
+
+[System.Serializable]
+public class Properties
+{
+    public Period[] periods;
+}
+
+[System.Serializable]
+public class Period
+{
+    public string name;
+    public string startTime;
+    public string endTime;
+    public int temperature;
+    public string temperatureUnit;
+    public string windSpeed;
+    public string windDirection;
+    public string shortForecast;
+    public string icon;
+}
